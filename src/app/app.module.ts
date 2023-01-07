e0d3aaf887a5e3a45cc1e3854b0ee5ece8f2f321
LandyYaoo <llpayaoo@hotmail.com>
2023-01-07 18:43:10 -0500

    sin puntos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { OurPlansComponent } from './components/our-plans/our-plans.component';
+import { SwiperModule } from "Swiper/angular";
+import { Component } from '@angular/core';
+import SwiperCore, { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactComponent,
+    GaleryComponent,
+    OurPlansComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+SwiperCore.use([Autoplay, Pagination, Navigation, FreeMode, ]),
+SwiperModule
---
+++ src/app/components/our-plans/our-plans.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-plans',
+  templateUrl: './our-plans.component.html',
+  styleUrls: ['./our-plans.component.sass']
+})
+export class OurPlansComponent {
+
+}
